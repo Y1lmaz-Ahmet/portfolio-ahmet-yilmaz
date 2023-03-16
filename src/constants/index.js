0990c5c9be358c35ef4efe0ea8b1571042e1210f
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-03-16 13:01:45 +0100

    update description

--- src/constants/index.js
+++ src/constants/index.js
@@ -263,8 +263,7 @@ const projects = [
   },
   {
     name: "Movies website",
-    description:
-      "a game in which two players seek in alternate turns to complete a row, a column, or a diagonal with either three O's or three X's drawn in the spaces of a grid of nine squares; noughts and crosses",
+    description: "A next-13 movies website",
     tags: [
       {
         name: "REACT",
